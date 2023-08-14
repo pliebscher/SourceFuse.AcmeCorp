0268dbdd84e7d5240adb98b1a07fdbbf1dd7eb20
Philip Liebscher <11465292+pliebscher@users.noreply.github.com>
2023-08-14 16:38:58 -0700

    Added RestSharp for integration tests

--- AcmeCorp.WebApi.Tests/AcmeCorp.WebApi.Tests/ProductsControllerTests.cs
+++ AcmeCorp.WebApi.Tests/AcmeCorp.WebApi.Tests/ProductsControllerTests.cs
@@ -7,19 +7,10 @@ namespace AcmeCorp.WebApi.Tests
     [TestClass]
     public class ProductsControllerTests
     {
-        private readonly string _apiKey = "pgH7QzFHJx4w46fI~5Uzi4RvtTwlEXp";
-        private RestClient _restClient;
 
         [TestInitialize]
         public void Setup()
         {
-            // Adjust for environment...
-            // This should be in a config file that adjusted in the CI/CD pipeline...
-            //string apiUrl = "http://localhost:49162/"; // Docker
-            string apiUrl = "http://localhost:14627/"; // IIS Express
-
-            _restClient = new RestClient(apiUrl);
-            _restClient.AddDefaultHeader("XApiKey", _apiKey);
 
         }
 
@@ -28,8 +19,9 @@ namespace AcmeCorp.WebApi.Tests
         {
 
             RestRequest request = new RestRequest("api/Products", Method.Get);
-            RestResponse response = _restClient.Execute(request);
+            RestResponse response = TestSetup.RestClient.Execute(request);
 
+            // We don't really care if there are any products or not so just make sure the call was a success.
             Assert.IsTrue(response.IsSuccessStatusCode);
 
         }
---
+++ AcmeCorp.WebApi.Tests/AcmeCorp.WebApi.Tests/TestSetup.cs
@@ -0,0 +1,34 @@
+﻿using RestSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AcmeCorp.WebApi.Tests
+{
+
+    [TestClass]
+    public class TestSetup
+    {
+        public static RestClient? RestClient { get; private set; }
+
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext context)
+        {
+            string _apiKey = "pgH7QzFHJx4w46fI~5Uzi4RvtTwlEXp";
+
+            // Adjust for environment...
+            // This should be in a config file that adjusted in the CI/CD pipeline...
+            //string apiUrl = "http://localhost:49162/"; // Docker
+            string apiUrl = "http://localhost:14627/"; // IIS Express
+
+            RestClient = new RestClient(apiUrl);
+            RestClient.AddDefaultHeader("XApiKey", _apiKey);
+
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+ 
+        }
+    }
+
+}
